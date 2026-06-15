3cafcf306583b480aacf63edfc09c3d29d5cba72
AlexWalkerGD <alexwalkerson@hotmail.com>
2026-06-15 21:56:14 +0100

    feat: ensure get dashboard function is protected

--- app/_data/get-dashboard/index.ts
+++ app/_data/get-dashboard/index.ts
@@ -1,9 +1,16 @@
 import { db } from "@/app/_lib/prisma";
 import { TransactionType } from "@prisma/client";
 import { TotalExpensePerCategory, TransactionPercentagePerType } from "./types";
+import { auth } from "@clerk/nextjs/server";
 
 const getDashboard = async (month: string) => {
+  const { userId } = await auth();
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+
   const where = {
+    userId,
     date: {
       gte: new Date(`2026-${month}-01`),
       lt: new Date(`2026-${month}-31`),
