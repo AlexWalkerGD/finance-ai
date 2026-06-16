9ee0eec165644195822c1eb682d2620a41aeadc9
AlexWalkerGD <alexwalkerson@hotmail.com>
2026-06-16 22:23:43 +0100

    feat: ensure user is redirected to stripe checkout when acquiring plan

---
+++ app/subscription/_components/acquire_plan_button.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Button } from "@/app/_components/ui/button";
+import { createStripeCheckout } from "../_action/create-stripe-checkout";
+import { loadStripe } from "@stripe/stripe-js";
+
+const AcquirePlanButton = () => {
+  const handleAcquirePlanClick = async () => {
+    const { sessionId } = await createStripeCheckout();
+
+    if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
+      throw new Error("Missing Stripe publishable key");
+    }
+    const stripe = await loadStripe(
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    );
+    if (!stripe) {
+      throw new Error("Failed to load Stripe");
+    }
+    await stripe.redirectToCheckout({ sessionId });
+  };
+  return (
+    <Button
+      className="w-full rounded-full font-bold"
+      onClick={handleAcquirePlanClick}
+    >
+      Assinar Plano
+    </Button>
+  );
+};
+
+export default AcquirePlanButton;
