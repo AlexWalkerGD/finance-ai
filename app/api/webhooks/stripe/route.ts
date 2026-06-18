eb6587056b6bccb7f5c7436b86182f339480396c
AlexWalkerGD <alexwalkerson@hotmail.com>
2026-06-19 00:18:28 +0100

    feat: add stripe webhook that handles subscriptions

---
+++ app/api/webhooks/stripe/route.ts
@@ -0,0 +1,68 @@
+import { clerkClient } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+export const POST = async (request: Request) => {
+  if (!process.env.STRIPE_WEBHOOK_SECRET || !process.env.STRIPE_SECRET_KEY) {
+    return NextResponse.error();
+  }
+
+  const signature = request.headers.get("stripe-signature");
+  if (!signature) {
+    return NextResponse.error();
+  }
+  const text = await request.text();
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: "2024-10-28.acacia",
+  });
+
+  const event = stripe.webhooks.constructEvent(
+    text,
+    signature,
+    process.env.STRIPE_WEBHOOK_SECRET,
+  );
+
+  switch (event.type) {
+    case "invoice.paid": {
+      const invoice = event.data.object;
+
+      const customer = invoice.customer;
+      const subscriptionId = invoice.parent?.subscription_details?.subscription;
+      const clerkUserId =
+        invoice.parent?.subscription_details?.metadata?.clerk_user_id;
+
+      const clerk = await clerkClient();
+      await clerk.users.updateUser(clerkUserId, {
+        privateMetadata: {
+          stripeCustomerId: String(customer),
+          stripeSubscriptionId: String(subscriptionId),
+        },
+        publicMetadata: {
+          subscriptionPlan: "premium",
+        },
+      });
+
+      break;
+    }
+    case "customer.subscription.deleted": {
+      const subscription = await stripe.subscriptions.retrieve(
+        event.data.object.id,
+      );
+      const clerkUserId = subscription.metadata.clerk_user_id;
+      if (!clerkUserId) {
+        return NextResponse.error();
+      }
+
+      await clerkClient().users.updateUser(clerkUserId, {
+        privateMetadata: {
+          stripeCustomerId: null,
+          stripeSubscriptionId: null,
+        },
+        publicMetadata: {
+          subscriptionPlan: null,
+        },
+      });
+    }
+  }
+  return NextResponse.json({ received: true });
+};
